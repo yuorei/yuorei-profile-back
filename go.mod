23bcf8706860ef340b9f8acd6acc2a86670dde9d
yuorei <108039575+yuorei@users.noreply.github.com>
2023-09-17 23:22:03 +0900

    mux追加

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	cloud.google.com/go/firestore v1.11.0
 	firebase.google.com/go v3.13.0+incompatible
+	github.com/gorilla/mux v1.8.0
 	google.golang.org/api v0.131.0
 )
 
--- main.go
+++ main.go
@@ -6,16 +6,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	r := mux.NewRouter()
 	client, err := fiestore.NewFirestoreClient()
 	if err != nil {
 		log.Fatalf("Failed to initialize Firestore: %v", err)
 	}
 	h := handler.NewHandler(client)
 
-	http.HandleFunc("/blog", h.GetBlogs)
+	r.HandleFunc("/blog", h.GetBlogs).Methods("GET")
+	http.Handle("/", r)
 	fmt.Println("起動")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
