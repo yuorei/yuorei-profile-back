83e41ab11ba46b35a8136ce981528f7d7e370f1d
yuorei <108039575+yuorei@users.noreply.github.com>
2023-09-17 22:46:53 +0900

    get blogs

---
+++ handler/handler.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"cloud.google.com/go/firestore"
+)
+
+type Handler struct {
+	client *firestore.Client
+}
+
+func NewHandler(client *firestore.Client) *Handler {
+	return &Handler{
+		client: client,
+	}
+}
+
+func (h *Handler) GetBlogs(w http.ResponseWriter, r *http.Request) {
+	// CORS対応: 必要なヘッダーを追加
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// Firestoreからデータを取得
+	collection := h.client.Collection("blog")
+	documents, err := collection.Documents(context.Background()).GetAll()
+	if err != nil {
+		log.Fatalf("Failed to retrieve documents: %v", err)
+	}
+
+	// レスポンスのJSONを作成
+	var results []map[string]any
+	for _, doc := range documents {
+		data := doc.Data()
+		results = append(results, data)
+	}
+
+	// JSONをクライアントに返す
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(results)
+}
