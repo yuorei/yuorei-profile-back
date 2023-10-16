74ed20b1ea1b597d4a1849aa528e5d1d1818b84d
yuorei <108039575+yuorei@users.noreply.github.com>
2023-10-16 16:51:42 +0900

    環境変数からPORTを取得

--- main.go
+++ main.go
@@ -5,17 +5,23 @@ import (
 	"blog/handler"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
-const port = ":8080"
+
 
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
 	}
 	r := mux.NewRouter()
 	client, err := fiestore.NewFirestoreClient()
@@ -28,5 +34,5 @@ func main() {
 	r.HandleFunc("/blog/{id}", h.GetBlogByID).Methods("GET")
 	http.Handle("/", r)
 	log.Println("起動" + port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
