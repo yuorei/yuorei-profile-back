f76fb585affa6847777d09f94ec5dd1289134634
yuorei <108039575+yuorei@users.noreply.github.com>
2023-10-16 17:18:27 +0900

    fix env lood

--- main.go
+++ main.go
@@ -11,12 +11,10 @@ import (
 	"github.com/joho/godotenv"
 )
 
-
-
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 	port := os.Getenv("PORT")
 	if port == "" {
